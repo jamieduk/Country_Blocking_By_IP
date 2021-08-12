42cc618ae7803d625b49ad546dd9f9bb9b65bb36
Jay <jamied_uk@hotmail.com>
2021-08-12 20:49:12 +0100

    Add files via upload

--- Allow_Ali_Express.sh
+++ Allow_Ali_Express.sh
@@ -2,6 +2,6 @@
 # Example Ali Express IP
 ip="47.254.177.101"
 echo ""
-iptables -A INPUT -s $ip -j ACCEPT
+sudo iptables -A INPUT -s $ip -j ACCEPT
 echo "$ip Allowed!"
 bash menu.sh
--- Allow_By_IP.sh
+++ Allow_By_IP.sh
@@ -2,7 +2,7 @@
 echo ""
 echo "Enter IP To Allow"
 read ip
-iptables -A INPUT -s $ip -j ACCEPT
+sudo iptables -A INPUT -s $ip -j ACCEPT
 echo "$ip Allowed!"
 echo ""
 bash menu.sh
